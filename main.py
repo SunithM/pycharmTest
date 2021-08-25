b4da684663f9855cf07a12662910a7ff6f4178b2
SunithM <sunithsuni09@gmail.com>
2021-08-25 15:20:43 +0530

    add to branch

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Git upload test')
     print('Second commit test')
+    print('Updating Branch')
 
 
 # Press the green button in the gutter to run the script.
